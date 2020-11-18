5f467e8cfc8a728056b42184f8e12838e1203e58
jzolo22 <jzolotarev@gmail.com>
2020-11-18 15:54:46 -0500

    kind of done

--- js/challenge.js
+++ js/challenge.js
@@ -23,8 +23,8 @@ let stop = 0
 
 
 const minus = minusButton.addEventListener("click", function(){
+    let number = parseInt(counter.textContent)
     if (stop == 0) {
-        let number = parseInt(counter.textContent)
         counter.textContent = number - 1
     } else {
         clearInterval(minus)
@@ -32,8 +32,8 @@ const minus = minusButton.addEventListener("click", function(){
 })
 
 const plus = plusButton.addEventListener("click", function(){
+    let number = parseInt(counter.textContent)
     if (stop == 0){
-        let number = parseInt(counter.textContent)
         counter.textContent = number + 1
     } else {
         clearInterval(plus)
@@ -42,16 +42,34 @@ const plus = plusButton.addEventListener("click", function(){
 
 const heart = heartButton.addEventListener("click", function(){
     if (stop == 0){
-        likes ++
         const ul = document.querySelector("ul.likes")
-        let number= counter.textContent
-        const li = document.createElement("li")
-        li.textContent = number + ` has ${likes}` + " Likes"
-        ul.append(li)
+        let number = counter.textContent
+        
+        if (ul.querySelector("li") != undefined){
+            const list = ul.querySelectorAll("li")
+            list.forEach(function(li){
+                if (li.textContent.split(" ")[0] == number){
+                    li.textContent = number + `${li.textContent.split(" ")[2]+1}` + "Likes"
+                } else {
+                    const newLi = document.createElement("li")
+                    newLi.textContent = number + "has 1 Likes"
+                    ul.append(newLi)
+                }
+            })
+        } else {
+            const newLi = document.createElement("li")
+                    newLi.textContent = number + "has 1 Likes"
+                    ul.append(newLi)
+        }
     } else {
         clearInterval(heart)
     }
 })
+
+// if a li in the ul exists with the current number 
+// then: check the likes amount on that li and increment by 1 
+// if it doesn't exist 
+// then: create an li in the ul with likes starting at 1
 
 pauseButton.addEventListener("click", function(){
     if (stop == 0){
@@ -76,3 +94,18 @@ form.addEventListener("submit", function(event){
     commentSection.append(pTag)
     event.target.reset()
 })
+
+
+
+// document.addEventListener("click", function(event){
+//     if (event.target.matches("button") && stop == 0){
+//         let number = parseInt(counter.textContent)
+//         if (event.target.matches("#minus")){
+//             counter.textContent = number - 1
+//         } else if (event.target.matches("#plus")){
+//             counter.textContent = number + 1
+//         } else {
+//             clearInterval
+//         }
+//     }
+// })
